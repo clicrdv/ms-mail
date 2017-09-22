a8bfc11daa7352fd454410011098f1a1f4de205e
mikrob <mikael.robert@clicrdv.com>
2017-09-22 13:58:26 +0200

    ruby client example

---
+++ client.rb
@@ -0,0 +1,29 @@
+this_dir = File.expand_path(File.dirname(__FILE__))
+lib_dir = File.join(this_dir, 'ruby-client')
+
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
+require 'grpc'
+require 'msmail_services_pb'
+
+def main
+  stub = Mailservice::ClicRdvMailService::Stub.new('localhost:50052', :this_channel_is_insecure)
+  mailstatus = stub.send_mail(
+    Mailservice::ClicRdvMail.new(
+      fromName: "Mikrob From",
+      fromEmail: "mikrob@clicrdv.com",
+      replyToName: "noreply@clicrdv.com",
+      replyToEmail: "noreply@clicrdv.com",
+      htmlContent: "<html><body><h1>This is a mail title </h1><b>This is bold content </b></i> This is italitc content</i></body></html>",
+      textContent: "this is a text content",
+      subject: "MS Mail subject",
+      toMap: {
+        "pipo" => "pipo@yopmail.com",
+        "molo" => "molo@yopmail.com"
+      }
+    )
+  ).message
+  p mailstatus
+end
+
+main
