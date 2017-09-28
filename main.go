f67bfca1cf1080ef6c37f457429bebb9a9460417
mikrob <mikael.robert@clicrdv.com>
2017-09-28 14:08:45 +0200

    add interaction with follow mail service, use env var for listen and remote address

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"strings"
 
 	"github.com/google/uuid"
 	context "golang.org/x/net/context"
 
 	"google.golang.org/grpc"
 
+	followpb "github.com/clicrdv/ms-grpc-stubs/followservice"
 	pb "github.com/clicrdv/ms-grpc-stubs/mailservice"
 	"github.com/clicrdv/ms-mail/mail"
 )
@@ -31,12 +34,36 @@ func (s *server) SendMail(ctxt context.Context, mailToSend *pb.ClicRdvMail) (*pb
 	}
 	status, uuid := sm.SendMail()
 	log.Print("Finished grpc processing of mail with uuid:", sm.UUID)
+
+	conn, err := grpc.Dial(os.Getenv("FOLLOW_MAIL_ADDRESS"), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	emails := []string{}
+	for _, value := range sm.ToMap {
+		emails = append(emails, value)
+	}
+	emailsStr := strings.Join(emails, ",")
+	clicRdvFollowMail := followpb.ClicRdvFollowMail{
+		Email:   emailsStr,
+		Event:   "SENT",
+		GroupId: "12345",
+		Uuid:    sm.UUID,
+	}
+	followStatusClient := followpb.NewClicRdvFollowMailServiceClient(conn)
+	sendMailStatus, err := followStatusClient.NotifySentMail(context.TODO(), &clicRdvFollowMail)
+	if err != nil {
+		log.Fatal("Error while sending mail status to follow mail service : ", err.Error())
+		log.Fatal("Status : ", sendMailStatus)
+	}
 	return &pb.SendMailStatus{Status: status, UniqueId: uuid}, nil
 }
 
 func main() {
 	log.Print("Starting microservice grpc listening on 3008")
-	lis, err := net.Listen("tcp", "0.0.0.0:3008")
+	listenAddress := os.Getenv("LISTEN_ADDRESS")
+	lis, err := net.Listen("tcp", listenAddress)
 	if err != nil {
 		log.Fatalf("Failed to listen : %v", err)
 	}
